f388d9b32a55e092663cd511ff3e40c2a36d78f7
Szurovecz János <szjani@szjani.hu>
2012-12-04 20:15:57 +0100

    initial commit

---
+++ src/lf4php/helpers/NOPLogger.php
@@ -0,0 +1,102 @@
+<?php
+/*
+ * Copyright (c) 2012 Szurovecz János
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace lf4php\helpers;
+
+use lf4php\Logger;
+
+/**
+ * @SuppressWarnings("unused")
+ * @author Szurovecz János <szjani@szjani.hu>
+ */
+class NOPLogger implements Logger
+{
+    const NAME = 'NOP';
+
+    private static $instance = null;
+
+    /**
+     * @return NOPLogger
+     */
+    public static function getNOPLogger()
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    protected function __construct()
+    {
+    }
+
+    public function getName()
+    {
+        return self::NAME;
+    }
+
+    public function debug($format, array $params = array())
+    {
+    }
+
+    public function error($format, array $params = array())
+    {
+    }
+
+    public function info($format, array $params = array())
+    {
+    }
+
+    public function trace($format, array $params = array())
+    {
+    }
+
+    public function warn($format, array $params = array())
+    {
+    }
+
+    public function isDebugEnabled()
+    {
+        return false;
+    }
+
+    public function isErrorEnabled()
+    {
+        return false;
+    }
+
+    public function isInfoEnabled()
+    {
+        return false;
+    }
+
+    public function isTraceEnabled()
+    {
+        return false;
+    }
+
+    public function isWarnEnabled()
+    {
+        return false;
+    }
+}
---
+++ tests/lf4php/helpers/LoggingTest.php
@@ -0,0 +1,83 @@
+<?php
+/*
+ * Copyright (c) 2012 Szurovecz János
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace lf4php\helpers;
+
+use lf4php\LoggerFactory;
+use PHPUnit_Framework_TestCase;
+use RuntimeException;
+
+/**
+ * @author Szurovecz János <szjani@szjani.hu>
+ */
+class LoggingTest extends PHPUnit_Framework_TestCase
+{
+    public static function setUpBeforeClass()
+    {
+        try {
+            LoggerFactory::getLogger('any');
+            self::fail('Exception should have been throwed!');
+        } catch (RuntimeException $e) {
+        }
+    }
+
+    public function setUp()
+    {
+        LoggerFactory::setILoggerFactory(new NOPLoggerFactory());
+    }
+
+    public function testLoggingNoException()
+    {
+        $testLogger1 = LoggerFactory::getLogger('test1');
+        $testLogger2 = LoggerFactory::getLogger('test2');
+        self::assertSame($testLogger2, $testLogger1);
+    }
+
+    public function testIsAnyEnabled()
+    {
+        $logger = LoggerFactory::getLogger('any');
+        self::assertFalse($logger->isDebugEnabled());
+        self::assertFalse($logger->isErrorEnabled());
+        self::assertFalse($logger->isInfoEnabled());
+        self::assertFalse($logger->isTraceEnabled());
+        self::assertFalse($logger->isWarnEnabled());
+    }
+
+    public function testGetName()
+    {
+        $logger = LoggerFactory::getLogger('any');
+        self::assertEquals(NOPLogger::NAME, $logger->getName());
+    }
+
+    public function testCallLogMethods()
+    {
+        $logger = LoggerFactory::getLogger('any');
+        $logger->debug(null);
+        $logger->error(null);
+        $logger->info(null);
+        $logger->trace(null);
+        $logger->warn(null);
+        // no exception
+        self::assertTrue(true);
+    }
+}
